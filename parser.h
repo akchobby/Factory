22a9d698390296e64b6f3ccfedab62d2c2fa30a1
anil-kumar.chavali@student.isae-supaero.fr <rsx09@RSX09>
2019-10-22 23:03:39 +0200

    Parser added

---
+++ parser.h
@@ -0,0 +1,33 @@
+#ifndef PARSER_H_
+#define PARSER_H_
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "common.h"
+
+typedef struct parsed_data {
+	char cmd[20];
+	short GPIO_nb;
+	bool GPIO_state;
+	bool GPIO_dir;
+	short GPIO_intres;
+	int led_nb;
+	bool led_state;
+	bool relay_state;
+	int alarm_nb;
+	int alarm_state[3];
+	int sensor_nb;
+	double temp_threshold;
+	double pres_threshold;
+	double humd_threshold;
+	}parsed_data_t;
+
+parsed_data_t* parse_packet();
+
+#endif /*PARSER_H_*/
---
+++ parser/README.txt
@@ -0,0 +1,3 @@
+To run test on parser function copy common.h a, common.c and parser files in directory and run
+
+make
