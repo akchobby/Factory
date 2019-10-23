d8ba3a8044c703fc7629f3f7ca311a23e4fb5898
MarcJM <marc.jm1922@gmail.com>
2019-10-23 18:39:01 +0200

    Headers update for integration

---
+++ Thread/include/client.h
@@ -0,0 +1,3 @@
+#include "packet_storage.h"
+int _send_message(char *ip_address, int type, char* message);
+int send_message(int type, PacketStorage message);
--- Thread/include/common.h
+++ Thread/include/common.h
@@ -9,10 +9,11 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <pthread.h>
-
-#ifdef MARC
+#include "packet_storage.h"
+#include <semaphore.h> 
+/*#ifdef MARC
 	#define SERV "192.168.47.47"
-	#define REV_1 "192.168.47.38"
+	#define REV_1 "192.168.47.28"
 	#define REV_2 "192.168.47.40"
 #elif ANT
 	#define SERV "192.168.47.38"
@@ -22,16 +23,17 @@
 	#define SERV "192.168.47.40"
 	#define REV_1 "192.168.47.38"
 	#define REV_2 "192.168.47.47"
-#endif
+#endif*/
 
 #define PORT 8080
 #define SA struct sockaddr
-#define MAX_THREADS 8
+#define MAX_THREADS 7
 
-#define MAX 80
 #define ERROR_NO_FACTORY -1
-#define GET 1
-#define POST 2
+
+#define FACTORY 0x02
+#define DASHBOARD 0x04
+#define ALL (FACTORY | DASHBOARD)
 
 typedef struct {
     int new_socket_fd;
@@ -41,15 +43,30 @@ typedef struct {
 } thread_t;
 
 typedef struct __attribute__((packed)) {
-    int device_id;
-    char ip_address[32];
-    char message[256];
+    //int device_id;
+    char org_ip_address[32];
+    char send_ip_address[32];
+    char message[64];
     int message_type;
 } packet_t;
 
+typedef struct {
+	char ip_address[14];
+	int type;
+	int isConnected;
+} device_t;
+
+int pointer;
+device_t list_devices[6];
+device_t dashboard_device;
+device_t device;
+
 char *getServerIP();
+void _getDeviceIP(char *command, char *ip);
 void *getDeviceIP(void *args);
 void *getDeviceID(void * arg);
+void init_communications(void);
 void command_handler(char *buffer);
+void packet_handler(packet_t *buffer);
 void make_packet(char *message, packet_t *package, int type);
 void unmake_packet(char *message, packet_t *package);
