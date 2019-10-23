686ccc523a614ba655fe2eeb88017aeb0ba0d225
Anil Kumar <anil-kumar.chavali@student.isae-supaero.fr>
2019-10-23 02:33:00 +0200

    Uncommented lines

--- unparser.h
+++ unparser.h
@@ -6,13 +6,13 @@
 #include <strings.h>
 
 #include "sensors.h"
-//#include "actuators.h"
-//#include "GPIO.h"
+#include "actuators.h"
+#include "GPIO.h"
 
 void response_period(sensor_data_t *data);
 void response_aperiodic_sensor(sensor_data_t *data);
-//void response_aperiodic_actuator(actuator_data_t *data);
-//void response_aperiodic_gpio(GPIO_RESPONSE *data);
+void response_aperiodic_actuator(actuator_data_t *data);
+void response_aperiodic_gpio(GPIO_RESPONSE *data);
 
 
 #endif /*UNPARSER_H_*/
